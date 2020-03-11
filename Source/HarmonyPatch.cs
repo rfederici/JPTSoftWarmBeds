d8055ead8797c0ed31b9427a50dc6add09409eb5
João Pedro Torres <jaopedro05@outlook.com>
2020-03-11 00:47:57 -0300

    Rebalanced BedRestEffectiveness for sleeping on the ground.

--- Source/HarmonyPatch.cs
+++ Source/HarmonyPatch.cs
@@ -432,4 +432,19 @@ namespace SoftWarmBeds
             Find.Selector.Select(spawnedBed, false, true);
         }
     }
+
+    //Preventing people without beds from using the default BedRestEffectiveness value (80%). Switching to the minimun value instead. 
+    [HarmonyPatch(typeof(Need_Rest), "TickResting")]
+    public class Need_Rest_Patch
+    {
+        public static bool Prefix(float restEffectiveness, Pawn ___pawn)
+        {
+            if (___pawn.RaceProps.Humanlike && ___pawn.CurrentBed() == null && restEffectiveness == StatDefOf.BedRestEffectiveness.valueIfMissing)
+            {
+                ___pawn.needs.rest.TickResting(StatDefOf.BedRestEffectiveness.minValue);
+                return false;
+            }
+            return true;
+        }
+    }
 }
